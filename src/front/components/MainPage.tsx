0337771da084448c8103539dfd7f396c463ce644
José Joaquín <jj.martinez87@hotmail.com>
2024-08-20 10:11:23 +0200

    add logout btn

--- src/front/components/MainPage.tsx
+++ src/front/components/MainPage.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "../styles/components/ToDo.css";
 import ToDoInput from "./ToDoInput";
 import ToDoList from "./ToDoList";
 
 const MainPage = () => {
+  const navigate = useNavigate();
   const [addedToDo, setAddedToDo] = useState<boolean>(false);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    navigate("/login");
+  };
 
   return (
     <section className="to-do-container flex flex-col items-center ">
@@ -12,6 +20,12 @@ const MainPage = () => {
       <article className="flex flex-col md:flex-row justify-between items-center mt-6">
         <ToDoList addedToDo={addedToDo} setAddedToDo={setAddedToDo} />
       </article>
+      <button
+        className="bg-[#855EDA] hover:bg-[#4A249D] text-[#fbe9c3] hover:text-[#F9E2AF] transition-all rounded-lg p-4 m-4"
+        onClick={handleLogout}
+      >
+        Log out
+      </button>
     </section>
   );
 };
--- src/front/hooks/useLogin.ts
+++ src/front/hooks/useLogin.ts
@@ -3,19 +3,23 @@ export const useLogin = async (
   password: string,
   navigate: any
 ) => {
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
-  if (response.ok) {
-    const { token } = await response.json();
-    sessionStorage.setItem("token", token);
-    navigate(`/not-today`);
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    if (response.ok) {
+      const { token } = await response.json();
+      sessionStorage.setItem("token", token);
+      navigate(`/not-today`);
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
